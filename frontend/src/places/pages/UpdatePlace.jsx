30380e1d58959ed1dee0cd8a643fd421c1592431
mohdshahzil <theshahzil@gmail.com>
2024-02-18 14:45:06 +0530

    initial-commit

---
+++ frontend/src/places/pages/UpdatePlace.jsx
@@ -0,0 +1,131 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+import Input from "../../shared/components/FormElements/Input";
+import Button from "../../shared/components/FormElements/Button";
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH,
+} from "../../shared/util/validators";
+
+import { useForm } from "../../shared/hooks/form-hook";
+import "./PlaceForm.css";
+
+// const DUMMY_PLACES = [
+//   {
+//     id: "p1",
+//     title: "Empire State Building",
+//     description: "One of the most famous sky scrapers in the world",
+//     imageUrl:
+//       "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Empire_State_Building_from_the_Top_of_the_Rock.jpg/1200px-Empire_State_Building_from_the_Top_of_the_Rock.jpg",
+//     address: "20 W 34th St, New York, NY 10001",
+//     location: {
+//       lat: 40.7484405,
+//       lng: -73.9878531,
+//     },
+//     creator: "u1",
+//   },
+//   {
+//     id: "p2",
+//     title: "Empire State Building",
+//     description: "One of the most famous sky scrapers in the world",
+//     imageUrl:
+//       "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Empire_State_Building_from_the_Top_of_the_Rock.jpg/1200px-Empire_State_Building_from_the_Top_of_the_Rock.jpg",
+//     address: "20 W 34th St, New York, NY 10001",
+//     location: {
+//       lat: 40.7484405,
+//       lng: -73.9878531,
+//     },
+//     creator: "u2",
+//   },
+// ];
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous skyscrapers in the world",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Empire_State_Building_from_the_Top_of_the_Rock.jpg/1200px-Empire_State_Building_from_the_Top_of_the_Rock.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878531,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Statue of Liberty",
+    description: "A symbol of freedom and democracy",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Statue_of_Liberty,_October_2016.jpg/1200px-Statue_of_Liberty,_October_2016.jpg",
+    address: "Liberty Island, New York, NY 10004",
+    location: {
+      lat: 40.689247,
+      lng: -74.044502,
+    },
+    creator: "u2",
+  },
+];
+const UpdatePlace = () => {
+  const placeId = useParams().placeId;
+  const identifiedPlace = DUMMY_PLACES.find((p) => p.id === placeId);
+
+  const [formState, inputHandler] = useForm(
+    {
+      title: {
+        value: identifiedPlace.title,
+        isValid: true,
+      },
+      description: {
+        value: identifiedPlace.description,
+        isValid: true,
+      },
+    },
+    true
+  );
+
+  const placeUpdateSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(formState.inputs);
+  };
+
+  if (!identifiedPlace) {
+    return (
+      <div className="center">
+        <h2>Could not find place!</h2>
+      </div>
+    );
+  }
+  return (
+    <form className="place-form" onSubmit={placeUpdateSubmitHandler}>
+      <Input
+        id="title"
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title"
+        onInput={inputHandler}
+        initialValue={formState.inputs.title.value}
+        initialValid={formState.inputs.title.isValid}
+      ></Input>
+      <Input
+        id="description"
+        element="textarea"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description"
+        onInput={inputHandler}
+        initialValue={formState.inputs.description.value}
+        initialValid={formState.inputs.description.isValid}
+      ></Input>
+      <Button type="submit" disabled={!formState.isValid}>
+        UPDATE PLACE
+      </Button>
+    </form>
+  );
+};
+
+export default UpdatePlace;
